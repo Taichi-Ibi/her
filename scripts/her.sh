41c99ae5ba3c0d80402995465adf8f54deb9ef33
Taichi-Ibi <t.ibi@estyle-inc.jp>
2024-09-11 00:51:28 +0900

    fastAPIに変更

--- scripts/her.sh
+++ scripts/her.sh
@@ -1,4 +1,5 @@
 model_content_path="$(dirname "$(dirname "$0")")/logs/model_response.md"
 query="{\"model_alias\":\"$1\",\"user_prompt\":\"$2\"}"
-curl -s -X POST -H "Content-Type: application/json" -d "$query" http://127.0.0.1:5000/ > "$model_content_path"
+curl -s -X POST -H "Content-Type: application/json" -d "$query" http://127.0.0.1:8000/ \
+    | jq -r ".content" > "$model_content_path"
 glow "$model_content_path"
---
+++ src/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+from src.her import Her
+from src.message import Message
+from src.model import ModelIdentifier
+
+app = FastAPI()
+
+class UserRequest(BaseModel):
+    model_alias: str
+    user_prompt: str
+
+@app.get("/")
+async def home():
+    return "Welcome to the FastAPI app!"
+
+@app.post("/")
+async def process_request(request: UserRequest):
+    try:
+        model_id = ModelIdentifier(model_alias=request.model_alias)
+        her = Her()
+        model_message: Message = her.invoke(
+            model_id=model_id, user_prompt=request.user_prompt
+        )
+        return model_message.__dict__
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
